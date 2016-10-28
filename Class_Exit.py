e88e6aee96f904b86e5ac7cc26e825f55f7ac152
kkw2238 <kkw2238@naver.com>
2016-10-28 21:55:06 +0900

    Delete Class_Exit.py

--- Class_Exit.py
+++
@@ -1,9 +0,0 @@
-from pico2d import *
-
-class Exit:
-    def __init__(self):
-        self.image = load_image('Door.png')
-        self.Open = True
-
-    def draw(self):
-        self.image.clip_draw(48 * self.Open , 0 , 48 , 48 , 850 , 170 + 24)
